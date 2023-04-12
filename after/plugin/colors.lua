a95ab2835e23a26c7abdf18f23d442069b1a9f45
Ngoc Nguyen <ngoc@ngocoder.com>
2023-04-13 06:40:08 +0700

    fix: github theme
    The `theme_style` is deprecated. Update GitHub theme syntax using the neovim `colorscheme`.

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,5 +1,6 @@
 require('github-theme').setup({
     function_style = "italic",
     sidebars = {"qf", "vista_kind", "terminal", "packer"},
-    theme_style = "light"
 })
+
+vim.cmd('colorscheme github_light')

0a42aef14e3edaaf5f9a7d3696f1e1536697def0
Ngoc Nguyen <ngoc@ngocoder.com>
2023-11-27 10:03:56 +0700

    fix(colorscheme): glitch colorscheme
    The time chunks are not sorted in chronological order causes the glitch
    when open NeoVim.
    
    Sort the time chunks by start time fixes the issue.

--- after/plugin/colorscheme.lua
+++ after/plugin/colorscheme.lua
@@ -14,18 +14,18 @@ require('nightfox').setup({
 -- Change colorscheme based on time of day
 function set_colorscheme(current_time, colorscheme_day_time_tbl)
     local colorscheme = nil
-    local last_colorscheme = nil
-    local last_time_chunk = { start_hour = -1, start_min = -1 }
-    for k, v in pairs(colorscheme_day_time_tbl) do
-        -- find the colorscheme of the current time chunk
-        if current_time.hour > v.start_hour or (current_time.hour == v.start_hour and current_time.min > v.start_min) then
-            colorscheme = v.colorscheme
-        end
-        
-        -- find the last time chunk
-        if last_time_chunk.start_hour < v.start_hour or (last_time_chunk.start_hour == v.start_hour and last_time_chunk.start_min < v.start_min) then
-            last_colorscheme = v.colorscheme
-            last_time_chunk = v
+    local time_chunks = {}
+    for _, v in pairs(colorscheme_day_time_tbl) do
+        table.insert(time_chunks, v)
+    end
+
+    -- sort the time chunks by start time
+    table.sort(time_chunks, function(a, b)
+        return a.start_hour < b.start_hour or (a.start_hour == b.start_hour and a.start_min < b.start_min)
+    end)
+    for _, tc in ipairs(time_chunks) do
+        if current_time.hour > tc.start_hour or (current_time.hour == tc.start_hour and current_time.min > tc.start_min) then
+            colorscheme = tc.colorscheme
         end
     end
 
@@ -34,7 +34,8 @@ function set_colorscheme(current_time, colorscheme_day_time_tbl)
     -- @example day `start_time` = 6:00, night `start_time` = 18:00
     -- If `current_time` = 5:00, then it is still the night of yesterday. The `colorscheme` is `night`
     if colorscheme == nil then
-        colorscheme = last_colorscheme
+        local last_time_chunk = time_chunks[#time_chunks]
+        colorscheme = last_time_chunk.colorscheme
     end
     vim.cmd('colorscheme ' .. colorscheme)
 end

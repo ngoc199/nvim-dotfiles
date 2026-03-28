c599079e5188ff3f1add2d92d09587645f9ea6d6
Ngoc Nguyen <ngoc@ngocoder.com>
2026-03-28 16:07:06 +0700

    add csharp lsp

---
+++ lua/plugins/csharp.lua
@@ -0,0 +1,9 @@
+return {
+	"seblyng/roslyn.nvim",
+	ft = { "cs", "razor" },
+	---@module 'roslyn.config'
+	---@type RoslynNvimConfig
+	opts = {
+		-- your configuration comes here; leave empty for default settings
+	},
+}
